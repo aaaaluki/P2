2ab1f5996db5d4bb02494ac47d26460477748b41
lluc <luki00.lrb@gmail.com>
2022-03-12 21:51:47 +0100

    Scripts de evaluación modificados
    Estos scripts solo muestran los resultados finales separados por ";".
    Primero una linea con los nombres y despues otra con los valores.
    
    Solo muestra los resultados acumulados, no por cada fichero.

---
+++ scripts/run_vad_opt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Be sure that this file has execution permissions:
+# Use the nautilus explorer or chmod +x run_vad.sh
+
+# Write here the name and path of your program and database
+DIR_P2=$HOME/PAV/P2
+DB=$DIR_P2/db.v4
+CMD=$DIR_P2/bin/vad
+
+for filewav in $DB/*/*wav; do
+    # echo "**************** $filewav ****************"
+    if [[ ! -f $filewav ]]; then 
+	    echo "Wav file not found: $filewav" >&2
+	    exit 1
+    fi
+
+    filevad=${filewav/.wav/.vad}
+
+    $CMD -1 $1 -2 $2 -i $filewav -o $filevad || exit 1
+
+# Alternatively, uncomment to create output wave files
+#    filewavOut=${filewav/.wav/.vad.wav}
+#    $CMD $filewav $filevad $filewavOut || exit 1
+
+done
+
+scripts/vad_evaluation_opt.pl $DB/*/*lab
+
+exit 0
