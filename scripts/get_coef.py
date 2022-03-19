a4c98345f245dfe27ce5c04f8681ee7086d5ebea
lluc <luki00.lrb@gmail.com>
2022-03-19 10:41:44 +0100

    Modificación de get_coef
    Ahora hay mas opciones, y muestra cotas para las alfas.
    Tambien he unificado los metodos main y plot.

--- scripts/get_coef.py
+++ scripts/get_coef.py
@@ -11,7 +11,9 @@ import numpy as np
 from matplotlib import cm
 from progress.bar import Bar
 
-PLOT = True
+PLOT = False
+SAVE_DATA = False
+TO_OPTIMIZE = 'total'
 DIR = Path(os.path.realpath(__file__)).parent.parent.absolute()
 CMD = DIR.as_posix() + '/scripts/run_vad_opt.sh'
 
@@ -47,8 +49,10 @@ def plot(alpha1: np.ndarray, alpha2: np.ndarray) -> np.ndarray:
     filename = 'data_{}:{}:{}_{}:{}:{}.csv'.format(
         alpha1[0], alpha1[-1], len(alpha1),
         alpha2[0], alpha2[-1], len(alpha2))
-    file = open(filename, 'w')
-    file.write('{},{},{}\n'.format(na1, na2, na1*na2))
+    
+    if SAVE_DATA:
+        file = open(filename, 'w')
+        file.write('{},{},{}\n'.format(na1, na2, na1*na2))
 
     # Vars for finding max
     alpha1_max_idx = 0
@@ -62,25 +66,29 @@ def plot(alpha1: np.ndarray, alpha2: np.ndarray) -> np.ndarray:
             while result is None:
                 result = f(alpha1[i], alpha2[j])
 
-            if result['total'] > val_max:
-                val_max = result['total']
+            if result[TO_OPTIMIZE] > val_max:
+                val_max = result[TO_OPTIMIZE]
                 alpha1_max_idx = i
                 alpha2_max_idx = j
 
-            Z[j][i] = result['total']
+            Z[j][i] = result[TO_OPTIMIZE]
 
-            file.write('{},{},{}\n'.format(alpha1[i], alpha2[j], Z[j][i]))
+            if SAVE_DATA:
+                file.write('{},{},{}\n'.format(alpha1[i], alpha2[j], Z[j][i]))
 
             bar.next()
     bar.finish()
-    file.close()
+    if SAVE_DATA:
+        file.close()
 
     # Show maximum
     print('Max value: {} -> alpha1: {}, alpha2: {}'.format(val_max, alpha1[alpha1_max_idx], alpha2[alpha2_max_idx]))
     print('Alpha1: {} - {}'.format(alpha1[alpha1_max_idx - 1], alpha1[alpha1_max_idx + 1]))
     print('Alpha2: {} - {}'.format(alpha2[alpha2_max_idx - 1], alpha2[alpha2_max_idx + 1]))
 
-    return
+    if not PLOT:
+        return
+
     # Plot the surface.
     X, Y = np.meshgrid(alpha1, alpha2)
 
@@ -98,40 +106,8 @@ def plot(alpha1: np.ndarray, alpha2: np.ndarray) -> np.ndarray:
     return Z
 
 
-def main(alpha1, alpha2) -> None:
-    n = len(alpha1)*len(alpha2)
-    best_alphas = []
-    best_result = {'total':0.0}
-
-    bar = Bar('Progress', fill='#', suffix='%(percent).2f%% - elapsed:%(elapsed)ds  eta:%(eta)ds',
-                max=n)
-
-    errors = 0
-    for a1 in alpha1:
-        for a2 in alpha2:
-            bar.next()
-
-            result = None
-            while result is None:
-                result = f(a1, a2)
-
-            if result['total'] > best_result['total']:
-                best_alphas = [a1, a2]
-                best_result = result
-            
-    bar.finish()
-
-    print('errors: {} -> {}%'.format(errors, 100*errors/n))
-    print('alpha1 = {:.4f}'.format(best_alphas[0]))
-    print('alpha2 = {:.4f}'.format(best_alphas[1]))
-    print_results(result)
-
-
 if __name__ == '__main__':
-    alpha1 = np.linspace(3.78, 4.27, num=10)
-    alpha2 = np.linspace(8.1, 9.1, num=10)
+    alpha1 = np.linspace(0, 10, num=10)
+    alpha2 = np.linspace(0, 10, num=10)
 
-    if PLOT:
-        plot(alpha1, alpha2)
-    else:
-        main(alpha1, alpha2)
+    plot(alpha1, alpha2)
