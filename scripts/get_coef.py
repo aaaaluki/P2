00d502bc7b613374563be2a0c5286d7a2799005a
lluc <luki00.lrb@gmail.com>
2022-03-19 11:31:50 +0100

    Ahora get_coef.py hace varias busquedas
    Cuando termina de bucar unas alfas optimas vuelve a buscar en una zona
    mas pequeña, esto lo repite N_ITERS veces. Con solo 3 iteraciones de
    11x11 muestras da buenos resultados.

--- scripts/get_coef.py
+++ scripts/get_coef.py
@@ -14,9 +14,12 @@ from progress.bar import Bar
 PLOT = False
 SAVE_DATA = False
 TO_OPTIMIZE = 'total'
+N_SAMPLES = 11
+N_ITERS = 3
+
 DIR = Path(os.path.realpath(__file__)).parent.parent.absolute()
 CMD = DIR.as_posix() + '/scripts/run_vad_opt.sh'
-
+T_EXEC = 0.22    # Machine dependant (in seconds)
 
 def f(a1:float, a2:float) -> Dict[str, float]:
     cmd = [CMD, str(a1), str(a2)]
@@ -82,32 +85,42 @@ def plot(alpha1: np.ndarray, alpha2: np.ndarray) -> np.ndarray:
         file.close()
 
     # Show maximum
-    print('Max value: {} -> alpha1: {}, alpha2: {}'.format(val_max, alpha1[alpha1_max_idx], alpha2[alpha2_max_idx]))
-    print('Alpha1: {} - {}'.format(alpha1[alpha1_max_idx - 1], alpha1[alpha1_max_idx + 1]))
-    print('Alpha2: {} - {}'.format(alpha2[alpha2_max_idx - 1], alpha2[alpha2_max_idx + 1]))
+    alpha1_range = alpha1[alpha1_max_idx - 1], alpha1[alpha1_max_idx + 1]
+    alpha2_range = alpha2[alpha2_max_idx - 1], alpha2[alpha2_max_idx + 1]
+    alphas_range = (alpha1_range, alpha2_range)
 
-    if not PLOT:
-        return
+    print('Max value: {:.4f} % -> alpha1: {:.4f}, alpha2: {:.4f}'.format(val_max, alpha1[alpha1_max_idx], alpha2[alpha2_max_idx]))
+    print('Alpha1 Range: {:.4f} - {:.4f}'.format(alpha1_range[0], alpha1_range[1]))
+    print('Alpha2 Range: {:.4f} - {:.4f}'.format(alpha2_range[0], alpha2_range[1]))
 
-    # Plot the surface.
-    X, Y = np.meshgrid(alpha1, alpha2)
+    if PLOT:
+        # Plot the surface.
+        X, Y = np.meshgrid(alpha1, alpha2)
 
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.viridis,
-                        linewidth=0, antialiased=False)
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.viridis,
+                            linewidth=0, antialiased=False)
 
-    ax.zaxis.set_major_formatter('{x:.02f}')
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.xlabel('Alpha 1')
-    plt.ylabel('Alpha 2')
-    
-    plt.show()
+        ax.zaxis.set_major_formatter('{x:.02f}')
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.xlabel('Alpha 1')
+        plt.ylabel('Alpha 2')
+        
+        plt.show()
 
-    return Z
+    return alphas_range
 
 
 if __name__ == '__main__':
-    alpha1 = np.linspace(0, 10, num=10)
-    alpha2 = np.linspace(0, 10, num=10)
+    alpha1 = np.linspace(0, 10, num=N_SAMPLES)
+    alpha2 = np.linspace(0, 20, num=N_SAMPLES)
 
-    plot(alpha1, alpha2)
+    print('Time estimation: {:.2f} s'.format(T_EXEC* N_SAMPLES*N_SAMPLES*N_ITERS))
+
+    for i in range(N_ITERS):
+        print('Iteration {}/{}'.format(i, N_ITERS))
+
+        alphas_range = plot(alpha1, alpha2)
+        
+        alpha1 = np.linspace((alphas_range[0][0]), (alphas_range[0][1]), num=N_SAMPLES)
+        alpha2 = np.linspace((alphas_range[1][0]), (alphas_range[1][1]), num=N_SAMPLES)
